a5e6ebc41555057828b62d0465b992d2fa52b129
AlphaCoder147 <thescholar8101@gmail.com>
2023-05-01 01:40:16 +0530

    Sounds, NPC death and Aim

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ class Game:
         self.screen = pygame.display.set_mode(RES)
         self.clock = pygame.time.Clock()
         self.delta_time = 1
+        self.global_trigger = False
+        self.global_event = pg.USEREVENT + 0
+        pg.time.set_timer(self.global_event, 40)
         self.new()
         
     def new(self):
@@ -39,16 +42,22 @@ class Game:
         pygame.display.set_caption(f'{self.clock.get_fps() :.1f}')
     
     def draw(self):
+        #self.screen.fill('black')
         self.obj_render.draw()
         self.weapon.draw()
         #self.map.draw()
         #self.player.draw()
         
     def check_event(self):
+        self.global_trigger = False
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            elif event.type == self.global_event:
+                self.global_trigger = True
+                    
             self.player.single_fire_event(event)
     def run(self):
         while True:
--- npc.py
+++ npc.py
@@ -1,5 +1,6 @@
 from sprite_obj import *
 from random import randint, random, choice
+from math import *
 
 class NPC(AnimatedSprite):
     def __init__(
@@ -21,7 +22,117 @@ class NPC(AnimatedSprite):
         self.accuracy = 0.15
         self.alive = True
         self.pain = False
+        self.ray_cast_value = False
+        self.frame_cnt = 0
         
     def update(self):
         self.check_animation_time()
         self.get_sprite()   
+        self.run_logic()
+        #self.draw_raycast()
+    
+    def animate_death(self):
+        if not self.alive:
+            if self.game.global_trigger and self.frame_cnt < len(self.death_imgs) - 1:
+                self.death_imgs.rotate(-1)
+                self.image = self.death_imgs[0]
+                self.frame_cnt += 1
+    
+    def animate_pain(self):
+        self.animate(self.pain_imgs)
+        if self.animation_trigger:
+            self.pain = False
+            
+    def check_npc_hit(self):
+        if self.ray_cast_value and self.game.player.shot:
+            if HALF_WIDTH - self.sprite_half_width < self.screen_x < HALF_WIDTH + self.sprite_half_width:
+                self.game.sound.npc_pain.play()
+                self.game.player.shot = False
+                self.pain = True
+                self.health -= self.game.weapon.damage
+                self.check_health()
+    
+    def check_health(self):
+        if self.health < 1:
+            self.alive = False
+            self.game.sound.npc_death.play()            
+        
+    def run_logic(self):
+        if self.alive:
+            self.ray_cast_value = self.ray_cast_player_npc()
+            self.check_npc_hit()
+            if self.pain:
+                self.animate_pain()
+            else:    
+                self.animate(self.idle_imgs)
+        else:
+            self.animate_death()        
+                
+                
+    @property
+    def map_pos(self):
+        return int(self.x), int(self.y)
+    
+    def ray_cast_player_npc(self):
+        if self.player.map_pos == self.map_pos:
+            return True
+        wall_dist_v, wall_dist_h = 0, 0
+        player_dist_v, player_dist_h = 0, 0
+        ox, oy = self.game.player.pos
+        x_map, y_map = self.game.player.map_pos
+        ray_angle =self.theta
+        
+        sinA = sin(ray_angle)
+        cosA = cos(ray_angle)
+        
+        Yhor, dy = (y_map + 1,1) if sinA > 0 else (y_map - 1e-6, -1)
+        
+        depth_hor = (Yhor - oy) / sinA
+        Xhor = ox + depth_hor * cosA
+        
+        delta_depth = dy / sinA
+        dx = delta_depth * cosA
+        
+        for i in range(MAX_DEPTH):
+            tile_hor = int(Xhor), int(Yhor)
+            if tile_hor == self.map_pos:
+                player_dist_h == depth_hor
+                break
+            if tile_hor in self.game.map.world_map:
+                wall_dist_h = depth_hor
+                break
+            Xhor += dx
+            Yhor += dy 
+            depth_hor += delta_depth   
+        
+        Xvert , dx = (x_map + 1, 1) if cosA > 0 else (x_map - 1e-6, -1)
+        depth_vert = (Xvert - ox) / cosA
+        Yvert = oy +  depth_vert * sinA
+        
+        delta_depth = dx / cosA
+        dy = delta_depth * sinA            
+        
+        for i in range(MAX_DEPTH):
+            tile_vert = int(Xvert), int(Yvert)
+            if tile_vert == self.map_pos:
+                player_dist_v = depth_vert
+                break
+            if tile_vert in self.game.map.world_map:
+                wall_dist_v = depth_vert
+                break
+            Xvert+=dx
+            Yvert += dy
+            depth_vert += delta_depth 
+        player_dist = max(player_dist_v, player_dist_h)
+        wall_dist = max(wall_dist_h, wall_dist_v)    
+        if 0 < player_dist < wall_dist or not wall_dist:
+            return True
+        return False
+    
+    def draw_raycast(self):
+        pg.draw.circle(self.game.screen, 'red', (100 * self.x, 100 * self.y), 15)
+        if self.ray_cast_player_npc():
+            pg.draw.line(self.game.screen, 'orange', (100 * self.game.player.x, 100 * self.game.player.y),
+                         (100 * self.x, 100 * self.y), 2)
+        
+        
